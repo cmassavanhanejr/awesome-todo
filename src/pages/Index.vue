2bbd1f9343800a1424a3d5276710a2eaf7cbadfb
Carlos Massavanhane <cmassavanhanejr@gmail.com>
2020-05-03 10:38:19 +0200

    refactor(): updated project name

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,44 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page padding>
+    <input
+      v-model="message"
+      @keyup.esc="clearMessage"
+      @keyup.enter="alertMessage"
+      />
+      <!--@keyup_enter="alertMessage"-->
+    <button @click="clearMessage" >clear</button>
+    <h5 class="border-grey">{{message}}</h5>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  data () {
+    return {
+      message: 'Hello World Vue!'
+    }
+  },
+  methods: {
+    clearMessage () {
+      this.message = ''
+    },
+    alertMessage () {
+      alert(this.message)
+    }
+    /* handleKeyup (e) {
+      console.log(e)
+      if (e.keyCode === 27) {
+        this.clearMessage()
+      } else if (e.keyCode === 13) {
+        this.alertMessage()
+      }
+    } */
+  }
 }
 </script>
+
+<style>
+ .border-grey{
+   border: 1px solid grey;
+ }
+</style>
